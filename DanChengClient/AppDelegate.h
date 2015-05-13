377d1c5e27573c6a9221c75d05a9385c17cc74c0
高磊 <gaolei@gaoleideMacBook-Pro.local>
2015-05-13 19:39:08 +0800

    Initial Commit

---
+++ DanChengClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DanChengClient
+//
+//  Created by 高磊 on 15/5/13.
+//  Copyright (c) 2015年 高磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
